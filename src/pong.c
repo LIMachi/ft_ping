e9b2cf3191d232261d55f0298a873f06d875e92a
LIMachi <hmartzol@made-f0Cr8s1.clusters.42paris.fr>
2024-09-18 18:05:56 +0200

    todo list

--- src/pong.c
+++ src/pong.c
@@ -12,7 +12,7 @@
 
 #include "ft_ping.h"
 
-int			valid_pong(void *packet, ssize_t size)
+int	valid_pong(void *packet, ssize_t size)
 {
 	struct icmphdr	*head;
 
@@ -20,40 +20,42 @@ int			valid_pong(void *packet, ssize_t size)
 		return (0);
 	head = packet;
 	if (head->type == ICMP_ECHOREPLY && size >= sizeof(t_ping_packet))
-		return head->un.echo.id == getpid();
+		return (head->un.echo.id == getpid());
 	return (0);
 }
 
-t_exit_code	receive_pong(void) {
-	t_ping_msg buffer;
-	ssize_t received;
-	t_i64 delta;
-
-	struct msghdr msg_reader = {
+t_exit_code	receive_pong(void)
+{
+	t_ping_msg			buffer;
+	ssize_t				received;
+	t_i64				delta;
+	const struct msghdr	msg_reader = {
 		.msg_iov = (struct iovec [1]){{
-			.iov_base = &buffer,
-			.iov_len = sizeof(buffer)
-		}},
-		.msg_iovlen = 1
-	};
+		.iov_base = &buffer,
+		.iov_len = sizeof(buffer)}},
+		.msg_iovlen = 1};
 
-	if ((received = recvmsg(app()->sock, &msg_reader, MSG_DONTWAIT)) == -1) {
-		if (errno != EAGAIN && errno != EWOULDBLOCK) {
+	received = recvmsg(app()->sock, &msg_reader, MSG_DONTWAIT);
+	if (received == -1)
+	{
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
+		{
 			//TODO: error
-			return app()->error = SOCKET_ERROR;
+			return (error(SOCKET_ERROR));
 		}
-		return OK;
+		return (OK);
 	}
-
-	if (received <= sizeof(struct iphdr)) {
+	if (received <= sizeof(struct iphdr))
+	{
 		//TODO: error
-		return app()->error = INVALID_MESSAGE;
+		return (error(INVALID_MESSAGE));
 	}
-	if (valid_pong(&buffer.packet, received - (ssize_t)sizeof(struct iphdr))) {
+	if (valid_pong(&buffer.packet, received - (ssize_t) sizeof(struct iphdr)))
+	{
 		delta = delta_time(buffer.packet.timestamp, now());
 		insert_pong(delta);
 		if (app()->flags != QUIET)
 			print_pong(received, delta, &buffer);
 	}
-	return OK;
+	return (OK);
 }
